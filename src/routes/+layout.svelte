2a8f857adc00fc7fd9efc6ad97ecd40c0984847a
Jenson-Bolton <jensonbolton@outlook.com>
2023-02-21 13:08:32 +0000

    Nav Started

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,28 @@
 <script>
 	import '../app.css';
+	import logo from '$lib/assets/logo.svg';
 </script>
 
-<nav />
+<header class="px-20 py-6 bg-white flex items-center justify-between text-nord-0">
+	<a href="/">
+		<div class="flex items-center">
+			<img src={logo} alt="logo" />
+			<h1 class="font-inter font-semibold text-3xl ml-4">Veles</h1>
+		</div>
+	</a>
+	<nav class="">
+		<ul>
+			<li class="inline"><a href="/">Home</a></li>
+			<li class="inline ml-3"><a href="/about-us">About Us</a></li>
+			<li class="inline ml-3"><a href="/products">Products</a></li>
+			<li class="inline ml-3 px-4 py-2 rounded border-nord-0 border-2">
+				<a href="/login">Login</a>
+			</li>
+			<li class="inline ml-3 px-3 py-2 rounded bg-nord-8 border-nord-8 border-2">
+				<a href="/sign-up">Sign Up</a>
+			</li>
+		</ul>
+	</nav>
+</header>
 
 <slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,9 @@ module.exports = {
 	content: ['./index.html', './src/**/*.{svelte,js,ts}'],
 	theme: {
 		fontFamily: {
-			sans: '"Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-serif	font-family: ui-serif, Georgia, Cambria, "Times New Roman", Times, serif; font-mono	font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
+			sans: '"Rubik", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-serif	font-family: ui-serif, Georgia, Cambria, "Times New Roman", Times, serif; font-mono	font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+			inter:
+				'"Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-serif	font-family: ui-serif, Georgia, Cambria, "Times New Roman", Times, serif; font-mono	font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
 		},
 		extend: {
 			colors: {
