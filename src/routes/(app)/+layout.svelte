aad1339d36fa404c0b9eb0e348e74fd0f07f421a
Jenson-Bolton <jensonbolton@outlook.com>
2023-03-05 02:35:43 +0000

    login pages done

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '../../app.css';
+	import { currentUser, pb } from '$lib/scripts/pocketbase';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
+
+	console.log(currentUser);
+
+	if (browser) {
+		console.log('hey');
+		if (currentUser === null) {
+			goto('/login');
+			console.log('hello');
+		} else {
+			console.log('boo');
+		}
+	}
+
+	/*pb.auth().onAuthStateChanged((user) => {
+		if (user === null) {
+			// User is not logged in, prompt to log in or create account
+		} else {
+			// User is logged in, proceed with app functionality
+		}
+	});*/
+</script>
+
+<slot />
